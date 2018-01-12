25bc2e79256402395308ed06113fce803af7bade
tselmeg <tselmeg9090@gmail.com>
2018-01-12 13:42:30 +0800

    before format

--- app/Http/Middleware/RedirectIfAuthenticated.php
+++ app/Http/Middleware/RedirectIfAuthenticated.php
@@ -18,7 +18,22 @@ class RedirectIfAuthenticated
     public function handle($request, Closure $next, $guard = null)
     {
         if (Auth::guard($guard)->check()) {
-            return redirect('/');
+            if ($user = Auth::user()->fitnessUser->isEmpty()){
+                return redirect('/home');
+            } else {
+                if($user = Auth::user()->fitnessUser->where('role_id', 1)->first()) {
+                    session()->put(['fitness_id'=>$user->fitness_id]);
+                    return redirect('/admin');
+                } elseif ($user = Auth::user()->fitnessUser->where('role_id', 2)->first()) {
+                    session()->put(['reception_fitness_id'=>$user->fitness_id]);
+                    return redirect('/reception');
+                } elseif ($user = Auth::user()->fitnessUser->where('role_id', 3)->first()) {
+                    session()->put(['trainer_fitness_id'=>$user->fitness_id]);
+                    return redirect('/trainer');
+                } else {
+                    return redirect('/home');
+                }
+            }
         }
 
         return $next($request);
